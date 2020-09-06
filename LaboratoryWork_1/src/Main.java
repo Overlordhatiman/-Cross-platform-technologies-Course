c1067c95df7a016081dac6547253732fe356374e
overlordhatiman <overlordhatiman@gmail.com>
2020-09-06 11:44:01 +0300

    initial commit

---
+++ LaboratoryWork_1/src/Main.java
@@ -0,0 +1,38 @@
+/**
+ * @author Kapinos Daneel
+ * @version 1.0
+ * 
+ */
+
+
+public class Main {
+
+	/**
+	 * Function for Laboratory work 1 
+	 * @param arguments - console arguments
+	 */
+	public static void main ( String [] arguments )
+    {
+        System.out.println("Group: 535st1");
+        System.out.println("Student: Kapinos Daneel Romanovich");
+        System.out.println("Option: 7");
+        
+        String[] initials = {
+        "xx     xx xx xx         xx xx	 	",
+        "xx    xx  xx   xx       xx  xx  	",
+        "xx   xx   xx      xx    xx   xx 	",
+        "xx  xx    xx        xx  xx  xx     ",
+        "xxxx      xx        xx  xx xx		",
+        "xx xx     xx        xx  xx  xx	 	",
+        "xx  xx    xx     xx     xx   xx 	",
+        "xx   xx   xx   xx       xx    xx 	",
+        "xx    xx  xx xx         xx     xx 	"
+        };
+
+        for (String s : initials) {
+            System.out.println(s);
+        }
+        
+    }
+	
+}
